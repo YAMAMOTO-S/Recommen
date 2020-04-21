8f3a8a2b89ff37d0c67c1c691dfe2ebe948fae68
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-04-21 17:53:47 +0900

    Setup User and Materialize

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_063043) do
+ActiveRecord::Schema.define(version: 2020_04_21_084705) do
 
   create_table "notes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,18 @@ ActiveRecord::Schema.define(version: 2020_04_18_063043) do
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_notes_on_category_id"
     t.index ["user_id"], name: "index_notes_on_user_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
