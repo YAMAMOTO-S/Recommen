75b795ea79635fb739218d892c1d969d809a5578
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-04-22 09:28:03 +0900

    Add nickname to User

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -26,5 +26,4 @@ class NotesController < ApplicationController
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
-  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_084705) do
+ActiveRecord::Schema.define(version: 2020_04_22_000603) do
 
   create_table "notes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_04_21_084705) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
