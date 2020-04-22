f8ff3add237264571081cdb05842984440ac623f
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-04-22 11:05:48 +0900

    Fixed image_upload_function

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_04_22_000603) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "image", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
