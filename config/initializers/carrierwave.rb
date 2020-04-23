43528071e73854f1633b71043a15092745d0898e
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-04-23 15:55:31 +0900

    Set carrierwave for S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIAXW6JXB6E5N5LAN6R',
-      aws_secret_access_key: '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',
+      aws_access_key_id=Rails.application.credentials[:aws][:aws_access_key_id],
+      aws_secret_access_key=Rails.application.credentials[:aws][:aws_secret_access_key],
       region: 'ap-northeast-1'
     }
 
