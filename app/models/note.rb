04d1ba70990146046e206a067d380e3d890e1a86
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-04-28 16:10:43 +0900

    Set validation

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,8 @@
 class Note < ApplicationRecord
-  belongs_to :user
+  validates :title, presence: true
+  validates :image_url, presence: true
+  validates :content, presence: true
 
+  belongs_to :user
   mount_uploader :image_url, ImageUploader
 end
