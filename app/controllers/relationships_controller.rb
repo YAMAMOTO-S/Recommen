a928be6e8b7a33131b3b3d5324cb2dd596a124f7
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-05-05 13:38:42 +0900

    Create follow and follower pages

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,15 @@
 class RelationshipsController < ApplicationController
   def create
+    @user = User.find(params[:user_id])
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save
-    redirect_to new_note_path
+    redirect_to user_path(@user.id)
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_to root_path
+    redirect_to user_path(@user.id)
   end
 end
